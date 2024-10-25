df4743518c64b0c9b61d6c8ff57e9dcf4a35cbdf
A1ЭX <116053566+GeekInTheBedroom@users.noreply.github.com>
2024-10-25 23:55:28 +0700

    Create scroll_text.py
    Uploaded the code

---
+++ scroll_text.py
@@ -0,0 +1,27 @@
+import time
+import os
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def scroll_text(text: str, delay: float):
+    for i in range(len(text)):
+        print(text[i:])
+        time.sleep(delay)
+        clear_screen()
+
+    for i in range(len(text)):
+        spaces = ' ' * (len(text) - i)
+        print(f"{spaces}{text[:i]}")
+        time.sleep(delay)
+        clear_screen()
+
+clear_screen()
+while True:
+    scroll_text("ABCDEF", 0.5)
+
+"""
+If you can optimize this, please do it.
+I hope that this code could at least be used for your school project or something :)
+This is so useless. I don't even know why I did this.
+"""
